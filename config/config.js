45eb0547f8b0f088395b07f65fcd3c340e82d933
Sam Westigard <sjwestigard@gmail.com>
2020-04-30 01:03:39 -0700

    Fix macos conflixt

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
   nibbleHost:     process.env.HOST,
-  nibbleUser:     process.env.USER,
+  nibbleUser:     process.env.DBUSER,
   nibblePassword: process.env.PASSWORD,
   nibbleDatabase: process.env.DATABASE,
   AWSAccessKeyId: process.env.AWSAccessKeyId,
